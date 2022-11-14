664a575cfe2ea002b8ccc59b9bc6e7915f41a71a
Silvina Rodriguez <silvinaalerodriguez@gmail.com>
2022-11-14 19:20:42 -0300

    modificacion de los requerimientos

--- movie/models.py
+++ movie/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Movie(models.Model):
+    title = models.CharField(max_length=40)
+    description =  models.CharField(max_length=500)
+    genre =  models.CharField(max_length=40)
+    duration =  models.CharField(max_length=40)
+    image = models.ImageField(upload_to='movie', null=True, blank=True)
+    comments =  models.CharField(max_length=200)
