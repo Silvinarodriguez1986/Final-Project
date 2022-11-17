582ea965640caec6dfabe702347d5be47175b6bb
Silvina Rodriguez <silvinaalerodriguez@gmail.com>
2022-11-17 18:18:19 -0300

    nuevo template, form, modificacion del modelo. Hay que agregar el boton al css.

--- movie/urls.py
+++ movie/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 
 from movie import views 
 
+
 app_name = "movie"
 urlpatterns = [
     path("movies", views.movies, name="movie-list"),  
+    path("movie/add", views.create_movie, name="movie-add")
 ]
--- movie/views.py
+++ movie/views.py
@@ -1,14 +1,65 @@
 from django.shortcuts import render
 
+from datetime import datetime
+from django.contrib import messages
+from django.core.paginator import Paginator
+from django.http import HttpResponse
+from django.template import loader
 from movie.models import Movie
+from movie.forms import MovieForm 
 
-def movies(request):
+
+
+
+
+def get_movies(request):
     movies = Movie.objects.all()
+    paginator = Paginator(movies, 3)
+    page_number = request.GET.get("page")
+    return paginator.get_page(page_number)
 
-    context_dict = {"movies": movies}
 
+
+def create_movie(request):
+    if request.method == "POST":
+        movie_form = MovieForm(request.POST)
+        if movie_form.is_valid():
+            data = movie_form.cleaned_data
+            actual_objects = Movie.objects.filter(
+                title=data["title"], description=data["description"], genre=data["genre"], duration=data["duration"]
+            ).count()
+            print("actual_objects", actual_objects)
+            if actual_objects:
+                messages.error(
+                    request,
+                    f"La pelicula {data['title']} - {data['description']} - {data['genre']} - {data['duration']} ya está creado",
+                )
+            else:
+                movie = Movie(title=data["title"], description=data["description"], genre=data["genre"], duration=data["duration"])
+                movie.save()
+                messages.success(
+                    request,
+                     f"La pelicula {data['title']} - {data['description']} - {data['genre']} - {data['duration']} creado exitosamente",
+                )
+
+            return render(
+                request=request,
+                context={"movies": get_movies(request)},
+                template_name="movie/movie_list.html",
+            )
+
+    movie_form = MovieForm(request.POST)
+    context_dict = {"form": movie_form}
     return render(
         request=request,
         context=context_dict,
+        template_name="movie/movie_form.html",
+        
+    )
+
+def movies(request):
+    return render(
+        request=request,
+        context={"movies": get_movies(request)},
         template_name="movie/movie_list.html",
     )
