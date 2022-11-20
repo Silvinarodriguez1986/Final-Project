e4f8695614a913033b26604f9327645b5f063981
Silvina Rodriguez <silvinaalerodriguez@gmail.com>
2022-11-20 16:55:19 -0300

    nuevos template, views, forms, el modelo de comentarios aun no funciona

--- movie/models.py
+++ movie/models.py
@@ -1,14 +1,41 @@
+from django.contrib.auth.models import User
+from django.core.validators import MinLengthValidator
 from django.db import models
 from ckeditor.fields import RichTextField
 
 
 class Movie(models.Model):
-    title = models.CharField(max_length=40)
-    genre =  models.CharField(max_length=40)
-    duration =  models.IntegerField()
+    title = models.CharField(max_length=40, null=False, blank=False)
+    genre =  models.CharField(max_length=40, null=False, blank=False)
+    duration =  models.IntegerField(null=False, blank=False)
     description =  RichTextField(null=True, blank=True)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    comments = models.ManyToManyField(
+        User, through="Comment", related_name="comments_owned"
+    )
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True) 
 
+    class Meta:
+        unique_together = (
+            "title",
+            "genre",
+            "duration",
+            "description",
+        )
+        ordering = ["-created_at"]
+
     def __str__(self):
         return f"{self.title} - {self.genre} - {self.duration} - {self.description}"
+
+
+class Comment(models.Model):
+    text = models.TextField(
+        validators=[
+            MinLengthValidator(10, "El comentario debe ser mayor de 10 caracteres")
+        ]
+    )
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
--- movie/urls.py
+++ movie/urls.py
@@ -5,18 +5,13 @@ from movie import views
 
 app_name = "movie"
 urlpatterns = [
-    #path("movies/", views.movies, name="movie-list"),  
-    #path("movie/add/", views.create_movie, name="movie-add"),
-    #path('movie/<int:pk>/detail/', views.movie_detail, name='movie-detail'),
-    #path('movie/<int:pk>/update/', views.movie_update, name='movie-update'),
-    #path('movie/<int:pk>/delete/', views.movie_delete, name='movie-delete'),
-
-
-
     path("movies/", views.MovieListView.as_view(), name="movie-list"),
     path("movie/add/", views.MovieCreateView.as_view(), name="movie-add"),
     path("movie/<int:pk>/detail/", views.MovieDetailView.as_view(), name="movie-detail"),
     path("movie/<int:pk>/update/", views.MovieUpdateView.as_view(), name="movie-update"),
     path("movie/<int:pk>/delete/", views.MovieDeleteView.as_view(), name="movie-delete"),
+    path("comment/<int:pk>/add/", views.CommentCreateView.as_view(), name="comment-create"),
+    path("comment/<int:pk>/delete/", views.CommentDeleteView.as_view(), name="comment-delete"),
 ]
+
 
